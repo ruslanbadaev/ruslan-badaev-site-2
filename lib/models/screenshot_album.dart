1dff3a5d9381f3162e1d14e60ec6d96cf1f1bc85
ruslanbadaev <r.badaeff@gmail.com>
2021-06-29 22:15:14 +0300

    add screenshots links

--- lib/models/screenshot_album.dart
+++ lib/models/screenshot_album.dart
@@ -1,13 +1,13 @@
 class ScreenshotAlbum {
   final String name;
   final String description;
-  final List<String> tegs;
+  final List<String> tags;
   final List<String> screenshots;
 
   ScreenshotAlbum({
     this.name,
     this.description,
-    this.tegs,
+    this.tags,
     this.screenshots,
   });
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,99 @@
 import 'package:ruslan_badaev_site/models/screenshot_album.dart';
 
 class ApiService {
+  List<ScreenshotAlbum> album = [
+    ScreenshotAlbum(
+      name: 'InSmartAgro',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-60.userapi.com/impg/yMt4iCuzL19sCtK1iFHN5ul_a4cBVL0WFZMWkQ/RfDiz5jP22Q.jpg?size=720x1600&quality=96&sign=70c31d8307b6c0afdf50764193d8e608&type=album',
+        'https://sun9-61.userapi.com/impg/3MUhCQccactMmYs_cUg_t_VmAzrykaGW3M3q2A/kgHCSYPDwEE.jpg?size=720x1600&quality=96&sign=42ad83335ac6816fff783b4d39000426&type=album',
+        'https://sun9-36.userapi.com/impg/huR4FtwyheaGkXk1og0EqgO8_z5K-kEyTk3Odg/ozuE0pvL-gM.jpg?size=720x1600&quality=96&sign=466e7c588569e839bb9e32bbf020ca6e&type=album',
+        'https://sun9-15.userapi.com/impg/Jx7U38xGwvBukiWLSWCFyHiRq8OZ0yZKMvJx5Q/C0dvvEPxIwE.jpg?size=720x1600&quality=96&sign=10b1dd9172277dd082367e71f95e1016&type=album',
+        'https://sun9-50.userapi.com/impg/tuNjUlyYWi9zsPp5fo6esYGGhUPv9XSyMqQ_qg/o6XI5v22osw.jpg?size=720x1600&quality=96&sign=8cdafd1d9373662622868ccfc6aed392&type=album',
+        'https://sun9-9.userapi.com/impg/gK8iounDdBTGXEPtX7rjuOhfAmfDOhmOtoAFTQ/0XWTKhrmtcg.jpg?size=720x1600&quality=96&sign=47781a7767cbb22b489e11a31e348aac&type=album',
+        'https://sun9-11.userapi.com/impg/qGqoqh9ODxe4kdgOaSURJqPrH7KkfblkNiVbxw/ktG0_MIh-2w.jpg?size=720x1600&quality=96&sign=937165a0d177755a4a03087a2e7f81c3&type=album',
+      ],
+    ),
+    ScreenshotAlbum(
+      name: 'InSmartRain',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-35.userapi.com/impg/11H_ZD38hJhcLg6uiKktJDaOyDTmvWGh0KtH3A/CrNuwfRh2vg.jpg?size=720x1600&quality=96&sign=b5bd4703a817e72eabbecce336f12bff&type=album',
+        'https://sun9-40.userapi.com/impg/s6atOje-pMGCRfMybKPaDe5yIXmz4rOuRB5YNw/5ibDEf9YBWE.jpg?size=720x1600&quality=96&sign=7db78e30be0408c1907f4fa3c227f945&type=album',
+        'https://sun9-4.userapi.com/impg/RxGThdq5CRYtC0jSU9dTjFpk2d0PVUdZqd2WPw/Jumfms_Cr6o.jpg?size=720x1600&quality=96&sign=3d8a907970a99b34f958b14d8fe1f2e6&type=album',
+        'https://sun9-6.userapi.com/impg/_suGWK8H7VDnWXJdBjz9NE8h3K0untbfLbnB9w/MFw9xLge_CU.jpg?size=720x1600&quality=96&sign=c526eea778d7bfd8bf4fb6394a46fe6b&type=album',
+      ],
+    ),
+    ScreenshotAlbum(
+      name: 'Urban Control',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-73.userapi.com/impg/Y8v7eywZAZKhwKkPK7JUiw1YYsbtTpqXqpHf9Q/2GhiMTW5GF4.jpg?size=720x1600&quality=96&sign=e7f8dcbe3df26a45c3f4087d51328f4a&type=album',
+        'https://sun9-22.userapi.com/impg/8rjgF0nkfvo6Wi56v0qggK2TbmysNAMkpLe1MQ/NghwFPSNVo0.jpg?size=720x1600&quality=96&sign=ffd779f58007080563bd967e0c49d1a6&type=album',
+        'https://sun9-25.userapi.com/impg/s6ILKP9SdFPZKoduDZ1ogEmKSEjhVh__5fOHoA/JxOoTuWN8O0.jpg?size=720x1600&quality=96&sign=a457acb0e00e0f52c57c8ae49ac3b342&type=album',
+        'https://sun9-54.userapi.com/impg/Vo7nxLT0Rsx8TDjMmTOTWXv8DMjZ01a2lfeBTg/RBd6wSgojAY.jpg?size=720x1600&quality=96&sign=9da31d893c52995a08106977dcbe524d&type=album',
+        'https://sun9-67.userapi.com/impg/hC0QaPrpuRau49pQb9QpY4xHSUIYGEZuTcI_vA/ZQvWgxnGfJ0.jpg?size=720x1600&quality=96&sign=3b8615f82174692139752add85bf9611&type=album',
+      ],
+    ),
+    ScreenshotAlbum(
+      name: 'Urban Control',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-28.userapi.com/impg/GYCrR5AuBgSYpjs6lU2cwEwwxEaUfxb5xo10LA/yngwKOh9pPI.jpg?size=823x1600&quality=96&sign=b56d1ddc9df8e1e5563bfa40f992daaa&type=album',
+        'https://sun9-29.userapi.com/impg/AnixrpwEawhzFekF2CBO6Vwl5tlZcSERrbQrvQ/U5RduUwYLtM.jpg?size=800x1600&quality=96&sign=6c2e826ef6b2f36b00143322735896cf&type=album',
+        'https://sun9-49.userapi.com/impg/ZRk9KwUfKZuL5fyyJy7c4LTE5sPosNWYvZEcKg/F2OgrHEQIdc.jpg?size=800x1600&quality=96&sign=0591ec201e34454e512928bc69568d03&type=album',
+        'https://sun9-70.userapi.com/impg/mYlStvZzsGQHwTAJ4GUfEA37YbNo_DwWQWeNOA/eLDlfwC4DNw.jpg?size=824x1600&quality=96&sign=c5807d4da77b2e49c7ac9747f69526dc&type=album',
+      ],
+    ),
+    ScreenshotAlbum(
+      name: 'Interra - Кабинет Абонента',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-41.userapi.com/impg/p3uhVmkwZF9If2rIDjJ9fZmabb7U7EJ0lekJHw/4CtgQ6dgArU.jpg?size=800x1600&quality=96&sign=e046187a7dab4fa71f94a34f272a40f2&type=album',
+        'https://sun9-19.userapi.com/impg/lgYf6fMqz0b9VmFuGFaOOsCPVVZ7cevYTkcfTw/MZDsJ7baHSo.jpg?size=800x1600&quality=96&sign=fe6dc4f78db9554687c4d6e289dc2311&type=album',
+        'https://sun9-25.userapi.com/impg/3biGHgy74NC5TUFmdqQcjL8QP9RYdnCWs53lTQ/IxJFjPpA7qI.jpg?size=800x1600&quality=96&sign=36db790c5fec1958588deeb6c4f98f28&type=album',
+        'https://sun9-10.userapi.com/impg/2ZrMSxbfSfWFkkO8-YDkeOhL7UChy2QoNbL_aQ/V2-_HQkQSn0.jpg?size=800x1600&quality=96&sign=dc09562e7d8dd6016f6b6fd9d18a1269&type=album',
+        'https://sun9-68.userapi.com/impg/xnDG1q4q7Xd8qxFUSPFTRzoijMxSYev5OxRrNA/OkGfTDzi8rg.jpg?size=800x1600&quality=96&sign=1edade0a7c950b061d121546ab885c2a&type=album',
+      ],
+    ),
+    ScreenshotAlbum(
+      name: 'CatWorm Client',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-73.userapi.com/impg/Whhfo2KASgT35N7nrZAhiLl8-dlzLoIxad1UUQ/oWjhEenyd6k.jpg?size=720x1600&quality=96&sign=7b9328c0e5daf87b0b99c38d7dfd674f&type=album',
+        'https://sun9-74.userapi.com/impg/aIZu5KV-1ue9IyXZXOTfrgKEGbOA9KXq_pGF9g/IvZ_DzgPp6w.jpg?size=720x1600&quality=96&sign=6725b7ec6efc3950d77f1b1a98076db3&type=album',
+        'https://sun9-6.userapi.com/impg/7Uu15oqeu25OENbzYwiKqKxA98cKATu8YadkIw/1QvIpE6jZW4.jpg?size=720x1600&quality=96&sign=b80751dcccf0ed40d11ad2793e9dffcb&type=album',
+        'https://sun9-12.userapi.com/impg/rOf7OyfErSRu_n46Y9PZABX_psmArr6pnWqQCQ/v3r1_G0uMy4.jpg?size=720x1600&quality=96&sign=b35e46c8e982a09426cc99f1fa91c93b&type=album',
+      ],
+    ),
+    ScreenshotAlbum(
+      name: 'King of the Stone Jungle',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-70.userapi.com/impg/NxbFQBuaT8jAJjtIDaisPbdwZt1r6WgSUZAfFA/bfg3n-lcIno.jpg?size=486x1080&quality=96&sign=9a49343495fd80e5928a6cf4b7650d70&type=album',
+        'https://sun9-37.userapi.com/impg/6gUDXEq_FAkbcM7cYq3K1AztfiFMFMOiLjDeWw/fF_bBILWVj8.jpg?size=486x1080&quality=96&sign=5fe54d13f656ef0c184388bdbf67f3f0&type=album',
+        'https://sun9-86.userapi.com/impg/UuONe3sWO7p2NOeONM37Z6f1GDHloGHmtDq6zg/zqdNMANdql4.jpg?size=486x1080&quality=96&sign=f2da41045fc8fe9090b2ad1a016152ab&type=album',
+      ],
+    ),
+    ScreenshotAlbum(
+      name: 'King of the Stone Jungle',
+      description: '',
+      tags: [],
+      screenshots: [
+        'https://sun9-31.userapi.com/impg/YPQv4PGgK2JYmP2qrwzL6P7eEj2TrYrK_WHHow/-m47XRCOxRs.jpg?size=800x1600&quality=96&sign=ea9d09ec309a58552a462f68915abdc7&type=album',
+        'https://sun9-15.userapi.com/impg/NCha9Nu4E11iZAVUNljRpVM9ioMS4qjzNBiAew/oVeqG9pet18.jpg?size=800x1600&quality=96&sign=9d53299810d9b95cc5e451c6738b1374&type=album',
+        'https://sun9-29.userapi.com/impg/BaW2G3UdKCmOXSUTRB7dkzZicc-coWlvFutPag/HOuqZrqVHY0.jpg?size=825x1600&quality=96&sign=ab018b7f7cd73fdecfc623a5a806ac27&type=album',
+      ],
+    ),
+  ];
   List<ScreenshotAlbum> getAllItems() {
     return [];
   }
